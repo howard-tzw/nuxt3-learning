99901f2a71e34ff65871528ba1c5b2c46b8d0ae9
Howard <howard.tzw@gmail.com>
2023-02-02 18:01:32 +0800

    add error handling

---
+++ components/ButtonOOPS.vue
@@ -0,0 +1,14 @@
+<template>
+	<button
+		class="font-base mx-2 rounded-full bg-sky-500 px-4 py-2 text-xl text-white hover:bg-sky-600 focus:outline-none focus:ring-2 focus:ring-sky-400 focus:ring-offset-2"
+		@click="onClick"
+	>
+		ButtonOOPS
+	</button>
+</template>
+
+<script setup>
+const onClick = () => {
+	throw new Error('由 ButtonOOPS 元件，拋出一個錯誤！')
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -10,6 +10,22 @@
 			<div class="my-4 flex flex-col space-y-4">
 				<NuxtLink to="/config">前往 /config</NuxtLink>
 			</div>
+
+			<NuxtErrorBoundary>
+				<!-- 這個按鈕元件可以拋出一個錯誤 -->
+				<ButtonOOPS />
+
+				<template #error="{ error }">
+					<p class="my-4 text-xl text-gray-500">這裡是發生錯誤時，才會渲染出來的地方</p>
+					<button
+						type="button"
+						class="inline-flex items-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-green-700 hover:bg-green-200 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
+						@click="fixIssue(error)"
+					>
+						修正錯誤
+					</button>
+				</template>
+			</NuxtErrorBoundary>
 		</i-header>
 
 		<div class="flex p-10">
@@ -30,4 +46,8 @@
 	</div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const fixIssue = error => {
+	error.value = null
+}
+</script>
