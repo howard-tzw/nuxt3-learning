89108679263bf05ba660cb5c2f0f8223817ae80b
Howard <howard.tzw@gmail.com>
2023-02-02 11:02:53 +0800

    add pinia presistedstate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@inkline/nuxt', '@pinia/nuxt'],
+	modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@inkline/nuxt', '@pinia/nuxt', '@pinia-plugin-persistedstate/nuxt'],
 	typescript: {
 		typeCheck: false,
 	},
